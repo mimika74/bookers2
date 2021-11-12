83f848567d22f6cf83eb918a083dfc75aa483cb8
mimika74 <88548032+mimika74@users.noreply.github.com>
2021-11-12 13:31:06 +0000

    [Add]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0d5ff0d15497fe84cde88309f7072f314040ee880ab9e73e566e73d02a81330e5a19fac67762dd98b221d9f74d0b5f2c5091a30a07809d2b7e2a80f57911034b'
